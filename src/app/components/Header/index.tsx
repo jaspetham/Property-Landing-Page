0a046d43e6112ad1aa840209138bc1c2016616a3
jaspertham98 <jaspertham98@gmail.com>
2024-07-11 21:32:26 +0800

    update responsive menu

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import './Header.css';
 import SpecialButton from "../../shared/SpecialButton";
 
 const Header: React.FC = () => {
+    const [openNav, setOpenNav] = useState(false);
   return (
     <header className="p-4">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="mx-auto flex justify-between items-center gap-4">
         <div className="header-left  flex justify-between items-center">
-          <div className="text-lg font-bold flex justify-between logo">Puchong Freehold</div>
+          <div className="text-lg font-bold flex justify-between logo">
+            Freehold
+          </div>
           <nav className="nav-wrapper">
             <ul className="flex gap-6 font-bold uppercase">
               <li>
@@ -42,10 +47,15 @@ const Header: React.FC = () => {
               </li>
             </ul>
           </nav>
-
+          <div onClick={() => setOpenNav(!openNav)} id="nav-icon3" className={openNav ? 'open' : ''}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
         <div className="header-right float-right">
-          <SpecialButton text="Schedule a Visit" containerClass="fancy"/>
+          <SpecialButton text="Schedule a Visit" containerClass="fancy" />
         </div>
       </div>
     </header>
